738fcda1be5fd0a62fb0c08fad5e5a7551d118ee
Muhamad Anjar <arvanria@gmail.com>
2017-03-15 16:53:57 +0700

    add popover map

---
+++ src/pages/maps/PopoverMap.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+import { JaringanJalanFungsiMapEditPage } from './fungsiMapEdit';
+import { PopoverController,ViewController,NavController, NavParams } from 'ionic-angular';
+import { MapShareService } from '../../providers/mapshareservices';
+@Component({
+  template: `
+    <ion-list>
+      <button ion-item (click)="start()">Mulai Edit</button>
+      <button ion-item (click)="stop()">Berhenti Edit</button>
+      <button ion-item (click)="save()">Simpan</button>
+    </ion-list>
+  `,
+  providers:[MapShareService],
+})
+export class PopoverMap {
+  data:any;
+  fungsi:any;
+  constructor(private navParams: NavParams,public mapEdit: MapShareService,public navCtrl: NavController) {
+    console.log(this.navParams.data.data.fungsi);
+    if (this.navParams.data) {
+      this.data  = this.navParams.data.data
+      //this.fungsi  = this.navParams.data.fungsi; 
+    }
+    
+    this.mapEdit.getdrawing();
+    
+  }
+
+  start(){
+    this.mapEdit.setdrawing(true);
+    console.log(this.mapEdit.drawingmode);
+  }
+ 
+  stop(){
+    this.mapEdit.setdrawing(false);
+    console.log(this.mapEdit.drawingmode);
+  }
+  save(){
+    let id = this.data.fungsi.id;
+    let poly = this.data.poly;
+    let data = {shapeline:JSON.stringify(poly) };
+    
+    
+    this.mapEdit.PostEditMapFungsi(id,data).subscribe(data => {
+        this.data = data;
+        if(this.data[0].result =="success"){
+          
+          this.navCtrl.popToRoot();
+        }
+            
+    }, error => {
+      console.log("Oooops!");
+    });
+  }
+}
--- src/pages/maps/maps.ts
+++ src/pages/maps/maps.ts
@@ -19,6 +19,9 @@ var markers: Array<any>;
 var modalMap;
 var mapMinZoom = 12;
 var mapMaxZoom = 18;
+//Geolocation
+var secondChild;
+var animationInterval;
 
 Injectable()
 @Component({
@@ -159,13 +162,13 @@ export class MapsPage {
             fullscreenControl: false
         });
 
-        var geolocationDiv = document.createElement('div');
-        var geolocationControl = this.GeolocationControl(geolocationDiv, map);
-        map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(geolocationDiv);
+        this.initGelocation();
+
+        
         
         
         //this.LoadFasilitas();
-        this.LoadPoiPandeglang();
+        //this.LoadPoiPandeglang();
 
         
         
@@ -184,124 +187,10 @@ export class MapsPage {
    
   }
 
-  GeolocationControl(controlDiv, map) {
 
-      // Set CSS for the control button
-      var controlUI = document.createElement('div');
-      controlUI.style.backgroundColor = '#444';
-      controlUI.style.borderStyle = 'solid';
-      controlUI.style.borderWidth = '1px';
-      controlUI.style.borderColor = 'white';
-      controlUI.style.height = '28px';
-      controlUI.style.marginTop = '5px';
-      controlUI.style.cursor = 'pointer';
-      controlUI.style.textAlign = 'center';
-      controlUI.title = 'Click to center map on your location';
-      
-      controlDiv.appendChild(controlUI);
+  
 
-      // Set CSS for the control text
-      var controlText = document.createElement('div');
-      controlText.style.fontFamily = 'Arial,sans-serif';
-      controlText.style.fontSize = '10px';
-      controlText.style.color = 'white';
-      controlText.style.paddingLeft = '10px';
-      controlText.style.paddingRight = '10px';
-      controlText.style.marginTop = '8px';
-      controlText.innerHTML = 'Center map on your location';
-      controlUI.appendChild(controlText);
-
-      // Setup the click event listeners to geolocate user
-      google.maps.event.addDomListener(controlUI, 'click', this.geolocate);
-  }
-
-  geolocate() {
-      
-      //markerMylocation.setMap(null);
-      Geolocation.getCurrentPosition().then((position) => {
-        let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-        
-        let markerMylocation = new google.maps.Marker({
-            position: latLng,
-            title:"Lokasi saya saat ini!",
-            icon: 'assets/maps/you-are-here-2.png',
-            animation: google.maps.Animation.DROP,
-        });
-        
-        markerMylocation.setMap(map);
-        map.setCenter(latLng);
-        /*google.maps.event.addDomListener(markerMylocation, 'click', () =>{
-
-        });*/
-        
-
-      }, (err) => {
-        console.log(err);
-      });
-
-      let watch = Geolocation.watchPosition();
-      watch.subscribe((data) => {
-        // data can be a set of coordinates, or an error (if an error occurred).
-        // data.coords.latitude
-        // data.coords.longitude
-      });
-      
-  }
-
-  addYourLocationButton (map, marker) {
-      let controlDiv = document.createElement('div');
-
-      var firstChild = document.createElement('button');
-      firstChild.style.backgroundColor = '#fff';
-      firstChild.style.border = 'none';
-      firstChild.style.outline = 'none';
-      firstChild.style.width = '28px';
-      firstChild.style.height = '28px';
-      firstChild.style.borderRadius = '2px';
-      firstChild.style.boxShadow = '0 1px 4px rgba(0,0,0,0.3)';
-      firstChild.style.cursor = 'pointer';
-      firstChild.style.marginRight = '10px';
-      firstChild.style.padding = '0';
-      firstChild.title = 'Your Location';
-      controlDiv.appendChild(firstChild);
-
-      var secondChild = document.createElement('div');
-      secondChild.style.margin = '5px';
-      secondChild.style.width = '18px';
-      secondChild.style.height = '18px';
-      secondChild.style.backgroundImage = 'url(https://maps.gstatic.com/tactile/mylocation/mylocation-sprite-2x.png)';
-      secondChild.style.backgroundSize = '180px 18px';
-      secondChild.style.backgroundPosition = '0 0';
-      secondChild.style.backgroundRepeat = 'no-repeat';
-      firstChild.appendChild(secondChild);
-
-      google.maps.event.addListener(map, 'center_changed', function () {
-          secondChild.style['background-position'] = '0 0';
-      });
-
-      firstChild.addEventListener('click', function () {
-          var imgX = '0',
-              animationInterval = setInterval(function () {
-                  imgX = imgX === '-18' ? '0' : '-18';
-                  secondChild.style['background-position'] = imgX+'px 0';
-              }, 500);
-
-          if(navigator.geolocation) {
-              navigator.geolocation.getCurrentPosition(function(position) {
-                  var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-                  map.setCenter(latlng);
-                  clearInterval(animationInterval);
-                  secondChild.style['background-position'] = '-144px 0';
-              });
-          } else {
-              clearInterval(animationInterval);
-              secondChild.style['background-position'] = '0 0';
-          }
-      });
-
-      //controlDiv.index = 1;
-      map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(controlDiv);
-  }
+  
 
   addMarker(){
  
@@ -317,7 +206,7 @@ export class MapsPage {
   
   }
 
-  public addInfoWindow(marker, content){
+  addInfoWindow(marker, content){
     
     infoWindow = new google.maps.InfoWindow({
       content: content
@@ -414,6 +303,90 @@ export class MapsPage {
           });
         }
   }
+
+  initGelocation(){
+    var controlDiv = document.createElement('div');
+
+    var firstChild = document.createElement('button');
+    firstChild.style.backgroundColor = '#fff';
+    firstChild.style.border = 'none';
+    firstChild.style.outline = 'none';
+    firstChild.style.width = '28px';
+    firstChild.style.height = '28px';
+    firstChild.style.borderRadius = '2px';
+    firstChild.style.boxShadow = '0 1px 4px rgba(0,0,0,0.3)';
+    firstChild.style.cursor = 'pointer';
+    firstChild.style.marginRight = '10px';
+    firstChild.style.marginLeft = '10px';
+    firstChild.style.padding = '0';
+    firstChild.title = 'Your Location';
+    controlDiv.appendChild(firstChild);
+
+    secondChild = document.createElement('div');
+    secondChild.style.margin = '5px';
+    secondChild.style.width = '18px';
+    secondChild.style.height = '18px';
+    secondChild.style.backgroundImage = 'url(https://maps.gstatic.com/tactile/mylocation/mylocation-sprite-2x.png)';
+    secondChild.style.backgroundSize = '180px 18px';
+    secondChild.style.backgroundPosition = '0 0';
+    secondChild.style.backgroundRepeat = 'no-repeat';
+    firstChild.appendChild(secondChild);
+
+    google.maps.event.addListener(map, 'center_changed', function () {
+        secondChild.style['background-position'] = '0 0';
+    });
+    let par = this;
+    firstChild.addEventListener('click', function () {
+        var imgX = '0';
+        animationInterval = setInterval(function () {
+                imgX = imgX === '-18' ? '0' : '-18';
+                secondChild.style['background-position'] = imgX+'px 0';
+            }, 500);
+
+        par.geolocate();
+
+    });
+
+    //controlDiv.index = 1;
+    map.controls[google.maps.ControlPosition.LEFT_TOP].push(controlDiv);
+  }
+
+  geolocate() {
+      
+      //markerMylocation.setMap(null);
+      if(Geolocation){
+      Geolocation.getCurrentPosition().then((position) => {
+        let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+        
+        let markerMylocation = new google.maps.Marker({
+            position: latLng,
+            title:"Lokasi saya saat ini!",
+            icon: 'assets/maps/you-are-here-2.png',
+            animation: google.maps.Animation.DROP,
+        });
+        
+        markerMylocation.setMap(map);
+        map.setCenter(latLng);
+        /*google.maps.event.addDomListener(markerMylocation, 'click', () =>{
+
+        });*/
+      
+      }, (err) => {
+        console.log(err);
+      });
+
+      let watch = Geolocation.watchPosition();
+      watch.subscribe((data) => {
+        console.log(data);
+        // data can be a set of coordinates, or an error (if an error occurred).
+        // data.coords.latitude
+        // data.coords.longitude
+      });
+      }
+      
+  }
+
+
 
   openModal(id) {
     var characters = 
